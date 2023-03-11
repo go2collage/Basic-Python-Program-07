ad0a19b060ae381c44d3a8d409da4164fc34f745
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-11 22:34:22 +0530

    Add files via upload

---
+++ BP_P7.py
@@ -0,0 +1,15 @@
+# Basic Python Program
+
+# Value swap using Single variable
+
+a = int(input("Enter a value: "))
+b = int(input("Enter b value: "))
+
+print("Before swaping a= ",a," b= ", b)
+
+# Swaping 
+c = a
+a = b
+b = c
+
+print("After swaping a= ", a , " b= ",b)
